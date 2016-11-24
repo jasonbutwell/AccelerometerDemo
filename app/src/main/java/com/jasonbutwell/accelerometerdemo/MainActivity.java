7270f05bee6bac6415d9664a78a29dde28acdb1a
Jason Butwell <jasonbutwell@gmail.com>
2016-11-24 01:42:35 +0000

    Implemented sensor and sensor manager. Registered the sensor listener to output the values of the X,Y,Z axes to three TextViews.

--- app/src/main/java/com/jasonbutwell/accelerometerdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/accelerometerdemo/MainActivity.java
@@ -1,13 +1,43 @@
 package com.jasonbutwell.accelerometerdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor mySensor;
+    private SensorManager sm;
+
+    private TextView x, y, z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        x = (TextView)findViewById(R.id.textViewX);
+        y = (TextView)findViewById(R.id.textViewY);
+        z = (TextView)findViewById(R.id.textViewZ);
+
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mySensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sm.registerListener(this,mySensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        x.setText(String.valueOf(sensorEvent.values[0]));
+        y.setText(String.valueOf(sensorEvent.values[1]));
+        z.setText(String.valueOf(sensorEvent.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
